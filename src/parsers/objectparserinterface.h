2c14ec3e1b3b2771197f76b01ee8a54f07659e8d
Alexey Saprunov <workernight@yandex.ru>
2023-11-17 17:12:47 +0400

    chore: add parsers and builders interfaces

---
+++ src/parsers/objectparserinterface.h
@@ -0,0 +1,30 @@
+#ifndef AOB_OBJECT_PARSER_INTERFACE_H
+#define AOB_OBJECT_PARSER_INTERFACE_H
+
+#include <QMap>
+#include <QMultiMap>
+#include <QString>
+
+namespace ao_builder
+{
+class ObjectParserInterface
+{
+public:
+    struct IniFileKey
+    {
+        QString keyName{};
+        QString keyLocale{};
+        QVariant value{};
+    };
+
+    using Section  = QMultiMap<QString, IniFileKey>;
+    using Sections = QMap<QString, Section>;
+
+public:
+    virtual ~ObjectParserInterface() = default;
+
+public:
+    virtual Sections getSections() = 0;
+};
+
+} // namespace ao_builder
